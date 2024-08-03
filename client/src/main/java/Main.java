7bd1ba87a3443c98516af71891129a05154a960b
Your Name <matthmella@gmail.com>
2024-08-03 10:37:04 -0600

    main client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,42 @@
 import chess.*;
+import serverfacade.ServerFacade;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade s = new ServerFacade();
+        String baseURL = "http://localhost:8080";
+
+        String command = args[0];
+        switch (command) {
+            case "register":
+                if (args.length != 4) {
+                    System.out.println("Incorrect amount of arguments");
+                    break;
+                }
+
+                try {
+                    s.register(args[1], args[2], args[3], baseURL);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+
+            case "login":
+                if (args.length != 3) {
+                    System.out.println("Incorrect amount of arguments");
+                    break;
+                }
+
+                try {
+                    s.login(args[1], args[2], baseURL);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
     }
 }
